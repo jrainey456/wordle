c3d3903c480b4d4c73c92b8ffa1b455585c5eac6
John Rainey <jrainey456@gmail.com>
2022-06-27 14:20:52 -0500

    Added draft for wordle boxes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Card, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <h1>wordle website</h1>
+    <Container>
+      <header>West</header>
+      <div>
+        <Row>
+          <Col>
+            <Card id='1' className='defaultCard'>
+              <Card.Body>T</Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card id='1' className='defaultCard'>
+              <Card.Body>T</Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card id='1' className='defaultCard'>
+              <Card.Body>T</Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card id='1' className='defaultCard'>
+              <Card.Body>T</Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card id='1' className='defaultCard'>
+              <Card.Body>T</Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </Container>
   );
 }
 
